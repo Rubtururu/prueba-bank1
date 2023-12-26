a4d911c64238e9781a9dda8f3712cc4869dcad82
Rubtururu <59855904+Rubtururu@users.noreply.github.com>
2023-12-26 20:13:46 +0100

    Update web3.js

--- web3.js
+++ web3.js
@@ -24,6 +24,24 @@ async function connectToMetaMask() {
 const contractAddress = '0x29D1Bf24a79C7d1FB8681C83ab738fB78E39a660';
 const contractABI = [{"inputs":[{"internalType":"address","name":"_usdtAddress","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"claimDividends","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"creatorWallet","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"deposit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"dividendPool","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"getUserDailyDividends","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"isDepositor","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"lastDividendClaim","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalDeposits","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"treasuryPool","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"usdtAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"usdtToken","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"userDeposits","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"userDividends","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdrawDividends","outputs":[],"stateMutability":"nonpayable","type":"function"}];  // Coloca aquí el ABI del contrato
 
+// Conectar a la red BSC
+const web3 = new Web3('https://bsc-dataseed1.binance.org:443'); // Actualiza con el proveedor BSC correcto
+const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+async function updateUI() {
+    const totalDeposits = await contract.methods.totalDeposits().call();
+    const userDeposits = await contract.methods.userDeposits(userAddress).call();
+    const userDividends = await contract.methods.userDividends(userAddress).call();
+
+    document.getElementById('totalDeposits').innerText = totalDeposits.toString();
+    document.getElementById('userDeposits').innerText = userDeposits.toString();
+    document.getElementById('userDividends').innerText = userDividends.toString();
+}
+
+// Llamada inicial para actualizar la interfaz
+updateUI();
+
+
 // Conectar a la red Ethereum
 const provider = new ethers.providers.Web3Provider(web3.currentProvider);
 const signer = provider.getSigner();
