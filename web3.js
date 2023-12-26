6fbf5294b3393a3f65a3a19f627eec674b1d8f83
Rubtururu <59855904+Rubtururu@users.noreply.github.com>
2023-12-26 20:10:22 +0100

    Update web3.js

--- web3.js
+++ web3.js
@@ -1,3 +1,25 @@
+async function connectToMetaMask() {
+    try {
+        // Solicitar al usuario que conecte su cuenta MetaMask
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        
+        // Verificar si la conexión fue exitosa
+        if (window.ethereum.selectedAddress) {
+            console.log('Conectado a MetaMask:', window.ethereum.selectedAddress);
+
+            // Aquí puedes realizar operaciones adicionales después de la conexión
+            // Actualizar la interfaz, realizar llamadas al contrato, etc.
+
+            // Ejemplo: Llamar a la función updateUI después de la conexión
+            updateUI();
+        } else {
+            console.error('Error al conectar a MetaMask');
+        }
+    } catch (error) {
+        console.error('Error al solicitar cuentas a MetaMask:', error.message);
+    }
+}
+
 // Actualiza con la dirección del contrato y la red Ethereum que estás utilizando
 const contractAddress = '0x29D1Bf24a79C7d1FB8681C83ab738fB78E39a660';
 const contractABI = [{"inputs":[{"internalType":"address","name":"_usdtAddress","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"claimDividends","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"creatorWallet","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"deposit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"dividendPool","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"getUserDailyDividends","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"isDepositor","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"lastDividendClaim","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalDeposits","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"treasuryPool","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"usdtAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"usdtToken","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"userDeposits","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"userDividends","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdrawDividends","outputs":[],"stateMutability":"nonpayable","type":"function"}];  // Coloca aquí el ABI del contrato
