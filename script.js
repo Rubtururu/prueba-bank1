25ff0b12c1db72a424b779e8926349cd377fbcd8
Rubtururu <59855904+Rubtururu@users.noreply.github.com>
2023-12-26 21:44:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,95 +1,52 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    // Cambiar la dirección del contrato y otros detalles según sea necesario
-    const contractAddress = '0x29D1Bf24a79C7d1FB8681C83ab738fB78E39a660';
-    let userAddress;
+document.addEventListener("DOMContentLoaded", async () => {
+    // Replace with the deployed contract address
+    const contractAddress = "0x29D1Bf24a79C7d1FB8681C83ab738fB78E39a660";
 
-    // Conectar con MetaMask u otro proveedor web3
-     if (window.ethereum) {
-        window.web3 = new Web3(ethereum);
-        try {
-            await ethereum.enable();
-            const accounts = await web3.eth.getAccounts();
-            userAddress = accounts[0];
-            document.getElementById('userAddress').textContent = userAddress;
+    // Connect to the contract using ethers.js or web3.js
 
-            const contract = new web3.eth.Contract(contractAbi, contractAddress);
-            updateUI(contract);
-        } catch (error) {
-            console.error(error);
-        }
-    } else {
-        console.error('Web3 not found. Please install MetaMask.');
+    // Example with ethers.js
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+
+    // Update UI with initial values
+    updateUI();
+
+    // Event listeners for buttons
+    document.querySelector("button[data-action='deposit']").addEventListener("click", deposit);
+    document.querySelector("button[data-action='withdraw']").addEventListener("click", withdraw);
+    document.querySelector("button[data-action='claimDividends']").addEventListener("click", claimDividends);
+    document.querySelector("button[data-action='withdrawDividends']").addEventListener("click", withdrawDividends);
+
+    async function deposit() {
+        // Implement deposit logic
+        // Example: await contract.deposit(100);
+        updateUI();
+    }
+
+    async function withdraw() {
+        // Implement withdraw logic
+        // Example: await contract.withdraw();
+        updateUI();
+    }
+
+    async function claimDividends() {
+        // Implement claimDividends logic
+        // Example: await contract.claimDividends();
+        updateUI();
+    }
+
+    async function withdrawDividends() {
+        // Implement withdrawDividends logic
+        // Example: await contract.withdrawDividends();
+        updateUI();
+    }
+
+    async function updateUI() {
+        // Fetch and update total deposits, user deposits, user dividends, etc.
+        // Example: const totalDeposits = await contract.totalDeposits();
+        // Update HTML elements
+        // document.getElementById("totalDeposits").textContent = totalDeposits;
+        // Repeat for other values
     }
 });
-
-async function updateUI(contract) {
-    const totalDeposits = await contract.methods.totalDeposits().call();
-    const userDividends = await contract.methods.userDividends(userAddress).call();
-    const lastDividendClaim = await contract.methods.lastDividendClaim(userAddress).call();
-
-    document.getElementById('totalDeposits').textContent = totalDeposits;
-    document.getElementById('userDividends').textContent = userDividends;
-    document.getElementById('lastDividendClaim').textContent = new Date(lastDividendClaim * 1000).toLocaleString();
-}
-
-async function approve() {
-    const contract = new web3.eth.Contract(contractAbi, 'DIRECCION_DEL_CONTRATO_USDT');
-    const amountToApprove = web3.utils.toWei('1000000', 'ether'); // Ajusta según sea necesario
-
-    try {
-        await contract.methods.approve(contractAddress, amountToApprove).send({ from: userAddress });
-        console.log('Aprobación exitosa');
-    } catch (error) {
-        console.error('Error al aprobar:', error);
-    }
-    // Actualizar la interfaz después de la aprobación
-    const updatedContract = new web3.eth.Contract(contractAbi, contractAddress);
-    updateUI(updatedContract);
-}
-
-async function deposit() {
-    const depositAmount = document.getElementById('depositAmount').value;
-    if (depositAmount > 0) {
-        const contract = new web3.eth.Contract(contractAbi, contractAddress);
-
-        try {
-            await contract.methods.deposit(depositAmount).send({ from: userAddress });
-            updateUI(contract);
-        } catch (error) {
-            console.error(error);
-        }
-    }
-}
-
-async function withdraw() {
-    const contract = new web3.eth.Contract(contractAbi, contractAddress);
-
-    try {
-        await contract.methods.withdraw().send({ from: userAddress });
-        updateUI(contract);
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-async function claimDividends() {
-    const contract = new web3.eth.Contract(contractAbi, contractAddress);
-
-    try {
-        await contract.methods.claimDividends().send({ from: userAddress });
-        updateUI(contract);
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-async function withdrawDividends() {
-    const contract = new web3.eth.Contract(contractAbi, contractAddress);
-
-    try {
-        await contract.methods.withdrawDividends().send({ from: userAddress });
-        updateUI(contract);
-    } catch (error) {
-        console.error(error);
-    }
-}
