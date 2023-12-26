0a518fce3fa497501758a68413677d4e011ddf3f
Rubtururu <59855904+Rubtururu@users.noreply.github.com>
2023-12-26 21:47:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,70 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    // Replace with the deployed contract address
     const contractAddress = "0x29D1Bf24a79C7d1FB8681C83ab738fB78E39a660";
-
-    // Connect to the contract using ethers.js or web3.js
-
-    // Example with ethers.js
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     const contract = new ethers.Contract(contractAddress, abi, signer);
 
-    // Update UI with initial values
     updateUI();
 
-    // Event listeners for buttons
     document.querySelector("button[data-action='deposit']").addEventListener("click", deposit);
     document.querySelector("button[data-action='withdraw']").addEventListener("click", withdraw);
     document.querySelector("button[data-action='claimDividends']").addEventListener("click", claimDividends);
     document.querySelector("button[data-action='withdrawDividends']").addEventListener("click", withdrawDividends);
 
     async function deposit() {
-        // Implement deposit logic
-        // Example: await contract.deposit(100);
-        updateUI();
+        try {
+            // Implement deposit logic
+            // Example: await contract.deposit(100);
+            console.log("Deposit function called");
+            updateUI();
+        } catch (error) {
+            console.error("Error in deposit:", error);
+        }
     }
 
     async function withdraw() {
-        // Implement withdraw logic
-        // Example: await contract.withdraw();
-        updateUI();
+        try {
+            // Implement withdraw logic
+            // Example: await contract.withdraw();
+            console.log("Withdraw function called");
+            updateUI();
+        } catch (error) {
+            console.error("Error in withdraw:", error);
+        }
     }
 
     async function claimDividends() {
-        // Implement claimDividends logic
-        // Example: await contract.claimDividends();
-        updateUI();
+        try {
+            // Implement claimDividends logic
+            // Example: await contract.claimDividends();
+            console.log("Claim Dividends function called");
+            updateUI();
+        } catch (error) {
+            console.error("Error in claimDividends:", error);
+        }
     }
 
     async function withdrawDividends() {
-        // Implement withdrawDividends logic
-        // Example: await contract.withdrawDividends();
-        updateUI();
+        try {
+            // Implement withdrawDividends logic
+            // Example: await contract.withdrawDividends();
+            console.log("Withdraw Dividends function called");
+            updateUI();
+        } catch (error) {
+            console.error("Error in withdrawDividends:", error);
+        }
     }
 
     async function updateUI() {
-        // Fetch and update total deposits, user deposits, user dividends, etc.
-        // Example: const totalDeposits = await contract.totalDeposits();
-        // Update HTML elements
-        // document.getElementById("totalDeposits").textContent = totalDeposits;
-        // Repeat for other values
+        try {
+            // Implement updateUI logic
+            // Fetch and update total deposits, user deposits, user dividends, etc.
+            // Example: const totalDeposits = await contract.totalDeposits();
+            // Update HTML elements
+            // document.getElementById("totalDeposits").textContent = totalDeposits;
+            // Repeat for other values
+        } catch (error) {
+            console.error("Error in updateUI:", error);
+        }
     }
 });
